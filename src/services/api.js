3522e5a43c172f672709b3552801a33ca2effd11
nahdahvania <nahdah.vania17@gmail.com>
2025-05-09 14:47:01 +0700

    api nw

--- src/services/api.js
+++ src/services/api.js
@@ -4,7 +4,7 @@ export const getBerita = async () => {
   try {
     const response = await axios.get("http://localhost:8000/api/berita");
     console.log("RESPON API:", response.data);
-    return response.data.data; // hanya ambil bagian data array-nya
+    return response.data.data; 
   } catch (error) {
     console.error("Gagal mengambil data berita:", error);
     return [];
